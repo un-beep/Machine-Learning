434a38c82f19b3d2e89278dac88f98d0c4fb73de
un-beep <2724836758@qq.com>
2024-10-13 18:31:46 +0800

    modified:   .gitignore

--- 3线性模型/3.3/test.py
+++ 3线性模型/3.3/test.py
@@ -1,6 +1,19 @@
-import numpy as np
-import os
-a = np.array([[1],[2],[3]])
-b = np.array([[4],[5],[6]])
+import torch
 
-print(a*b)
+#查看torch版本
+print(torch.version)
+
+#查看cuda版本
+print(torch.version.cuda)
+
+#GPU是否可用
+print(torch.cuda.is_available())
+
+#返回gpu数量
+print(torch.cuda.device_count())
+
+#返回gpu名字，设备索引默认从0开始
+print(torch.cuda.get_device_name(0))
+
+#例子
+print(torch.rand(3,3).cuda())

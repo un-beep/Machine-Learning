cb836aacf21d43f8d5b249acf49efb15409c0e97
un-beep <2724836758@qq.com>
2024-11-03 16:33:11 +0800

    modified:   NILM/read_data.py 	new file:   NILM/state_classify.py

--- NILM/state_classify.py
+++ NILM/state_classify.py
@@ -4,18 +4,15 @@ import matplotlib.pyplot as plt
 
 path = r"NILM\Electricity_Data"
 # 1. 加载数据集
-# 有功功率
-P_Data = pd.read_csv(path + r'\Electricity_P.csv', parse_dates=True)
 # 电流
 I_Data = pd.read_csv(path + r'\Electricity_I.csv', parse_dates=True)
 
-P_Data.astype("int32")
 I_Data.astype("int32")
 
 # 2. 将 UNIX 时间戳转换为可读的日期格式
-P_Data['timestamp'] = pd.to_datetime(P_Data['UNIX_TS'], unit='s', errors='coerce')
-P_Data = P_Data.set_index(['timestamp'])
-P_Data.drop('UNIX_TS', axis=1, inplace=True)  # 删除 UNIX 时间戳列
+I_Data['timestamp'] = pd.to_datetime(I_Data['UNIX_TS'], unit='s', errors='coerce')
+I_Data = I_Data.set_index(['timestamp'])
+I_Data.drop('UNIX_TS', axis=1, inplace=True)  # 删除 UNIX 时间戳列
 
 # 3. 使用列名全称替换简写
 column_headers = {'WHE': "whole_house", 'RSE':'rental_suite', 'GRE' : 'garage','MHE': 'main_house', 
@@ -25,18 +22,18 @@ column_headers = {'WHE': "whole_house", 'RSE':'rental_suite', 'GRE' : 'garage','
                   'DNE': 'dining_room', 'EBE': 'workbench', 'FGE': 'fridge', 'HTE': 'hot_water', 
                   'OUE': 'outside', 'TVE': 'tv_pvr', 'UNE': 'unmetered'}
 
-P_Data = P_Data.rename(columns=column_headers)
+I_Data = I_Data.rename(columns=column_headers)
 
-P_Data.info(verbose=True)
+I_Data.info(verbose=True)
 
 
 # 3. 数据预处理
 # 检查缺失值
 print("缺失值检查：")
-print(P_Data.isnull().sum())
+print(I_Data.isnull().sum())
 
 # 填补缺失值
-P_Data.fillna(0, inplace=True)  # 或使用 data.dropna() 删除含缺失值的行
+I_Data.fillna(0, inplace=True)  # 或使用 data.dropna() 删除含缺失值的行
 
 # 4. 选取数据以供处理
 # appliances = ['main_house', 'clothes_washer', 'dish_washer', 'heat_pump', 
